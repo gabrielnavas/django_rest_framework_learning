eb40bfc4d092f57ccc82d49bd2e29851b43fe834
Navas <gabrielnavas@email.com>
2021-03-24 21:31:50 -0300

    authentication

--- products/views.py
+++ products/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Product
 from .serializers import ProductSerializer
 from . import services
 
 class ProductViewSet(viewsets.ViewSet):
+  
+  permission_classes = [IsAuthenticated]
+
   def create(self, request):
     try:
       productSerializer = ProductSerializer(data=request.data)
@@ -35,10 +40,24 @@ class ProductViewSet(viewsets.ViewSet):
     try:
       if not pk:
         return Response('need a product id', status=400)
-      product = get_object_or_404(Product, pk=pk)
+      product = Product.objects.get(pk=pk)
+      if not product:
+        return Response(status=404)
       serializer = ProductSerializer(product)
       return Response(data=serializer.data)
     except Exception as ex:
+      return Response(status=500)
+
+  def delete(self, request, pk=None):
+    try:
+      if not pk:
+        return Response('need a product id', status=400)
+      product = Product.objects.get(pk=pk)
+      product.delete()
+      return Response()
+    except Exception as ex:
+      if type(ex) == Product.DoesNotExist:
+        return Response(status=404)
       return Response(status=500)
 
 
